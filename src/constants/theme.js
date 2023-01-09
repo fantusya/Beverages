c87ea7c9957338426817283cb83e1796fb8f729a
Anastasiia <guba.anastaysha@gmail.com>
2023-01-09 13:05:13 +0200

    added theme of a site

---
+++ src/constants/theme.js
@@ -0,0 +1,77 @@
+export const theme = {
+  colors: {
+    mainText: '#000',
+    hero: '#fffafa',
+    white: '#fff',
+    sectionTitle: '#272f1f',
+    semiWhite: 'rgba(255, 255, 255, 0.3)',
+    filterBg: 'rgba(255, 255, 255, 0.15)',
+    red: '#B22222',
+    silver: '#C0C0C0',
+    semiSilver: 'rgba(192, 192, 192, 0.5)',
+    text: '#4682B4',
+    accent: '#F0F8FF',
+    secondary: '#b8bff7',
+    border: '#add8e6',
+    tableHead: 'rgba(240, 248, 255, 0.3)',
+    tableBody: 'rgba(240, 248, 255, 0.2)',
+    modalBg: 'rgba(36, 37, 99, 0.95)',
+    userNav: 'rgba(8, 19, 51, 0.5)',
+    error: '#D50000',
+    fallbackHero: 'rgba(0, 0, 0, 0.87)',
+    buttonBg: 'rgba(0, 0, 0, 0.5)',
+    transparent: 'transparent',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'Josefin Sans, sans-serif',
+    heading: 'Quicksand, sans-serif',
+    logo: 'Aboreto, cursive',
+  },
+  fontSizes: [
+    '12px', //0
+    '14px', //1
+    '16px', //2
+    '18px', //3
+    '20px', //4
+    '22px', //5
+    '24px', //6
+    '26px', //7
+    '32px', //8
+    '40px', //9
+    '50px', //10
+    '64px', //11
+    '70px', //12
+    '85px', //13
+    '90px', //14
+    '100px', //15
+    '120px', //16
+  ],
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    normal: 1.2,
+    heading: 1.5,
+    hero: 1.7,
+  },
+  letterSpacing: {
+    normal: '0.04em',
+    big: '0.08em',
+  },
+  borders: {
+    normal: '1px solid',
+    none: 'none',
+  },
+  radii: {
+    none: '0',
+    rounded: '15px',
+    moreRounded: '25px',
+    round: '50%',
+  },
+  transition: {
+    main: '400ms ease',
+  },
+  breakpoints: ['320px', '480px', '768px', '1200px'],
+};
